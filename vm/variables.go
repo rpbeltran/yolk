fb3392af3a1d10c50355f108d0bfafd43c9d9496
Ryan Beltran <rp.beltran@yahoo.com>
2025-01-01 21:41:03 -0500

    vm: support global variables

---
+++ vm/variables.go
@@ -0,0 +1,53 @@
+package vm
+
+import (
+	"fmt"
+	"yolk/types"
+)
+
+// FIXME: Currently only global variables are supported, support local variables soon
+
+func (vm *VirtualMachine) GetVariableID(name string) (uint64, error) {
+	if id, ok := vm.global_names[name]; !ok {
+		return 0, fmt.Errorf("could not find an associated id for the name: %q", name)
+	} else {
+		return id, nil
+	}
+}
+
+func (vm *VirtualMachine) FetchVariableById(id uint64) (types.Primitive, error) {
+	if object, ok := vm.globals[id]; !ok {
+		return nil, fmt.Errorf("could not find an object for the id: %d", id)
+	} else {
+		return object, nil
+	}
+}
+
+func (vm *VirtualMachine) FetchVariable(name string) (types.Primitive, error) {
+	if id, err := vm.GetVariableID(name); err != nil {
+		return nil, err
+	} else {
+		return vm.FetchVariableById(id)
+	}
+}
+
+func (vm *VirtualMachine) StoreNewVariable(name string, value types.Primitive) error {
+	if _, ok := vm.global_names[name]; ok {
+		return fmt.Errorf("variable %q cannot be redeclared", name)
+	}
+	new_id := vm.variable_id_counter
+	vm.variable_id_counter += 1
+
+	vm.global_names[name] = new_id
+	vm.globals[new_id] = value
+	return nil
+}
+
+func (vm *VirtualMachine) UpdateVariable(name string, value types.Primitive) error {
+	if id, err := vm.GetVariableID(name); err != nil {
+		return fmt.Errorf("could not update the variable %q due to the error: %v (perhaps it does not exist)", name, err)
+	} else {
+		vm.globals[id] = value
+		return nil
+	}
+}
