d18a6dee98dffa9274fa8ab5856e5c166ae645fd
Ryan Beltran <rp.beltran@yahoo.com>
2025-01-05 21:57:56 -0800

    main: add profiling flag

--- main.go
+++ main.go
@@ -3,15 +3,27 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
+	"runtime/pprof"
 	"yolk/cli"
 	"yolk/vm"
 )
 
 var runFlag = flag.String("run", "", ".yolk file to exec")
+var profilerFlag = flag.String("profiler", "", "start profiler and write the profile to the given path")
 var debugFlag = flag.Bool("debug", false, "Show vm state after each instruction")
 
 func main() {
 	flag.Parse()
+
+	if len(*profilerFlag) != 0 {
+		if f, err := os.Create(*profilerFlag); err != nil {
+			log.Fatalf("starting profiler: %v", err)
+		} else {
+			pprof.StartCPUProfile(f)
+		}
+	}
+
 	machine := vm.NewVM()
 	machine.MockExecutions = true
 
@@ -21,5 +33,9 @@ func main() {
 
 	if err := cli.ExecuteYolkFile(&machine, *runFlag, *debugFlag); err != nil {
 		log.Fatal(err)
+	}
+
+	if len(*profilerFlag) != 0 {
+		pprof.StopCPUProfile()
 	}
 }
